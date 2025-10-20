b35381ad677d832aec7db50370f7994388285171
krishpatel143x-netizen <krishpatel143x@gmail.com>
2025-10-20 19:31:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,36 @@
-import stremlit as stremlit
-from langchain.chat_models import ChatOpenAI
-from langchain.prompts import ChatPromptTemplate
+from langchain.agents import create_csv_agent
+from langchain.llms import OpenAI
+from dotenv import load_dotenv
+import os
+import streamlit as st
 
-st.title("Dhanushya - Market Analyis")
 
-#user imput
-user_imput = st.text_area("Enter your market data / question:")
+def main():
+    load_dotenv()
 
-if st.button("Analyze"):
-    if user_imput:
-        llm = ChatOpenAI(openai_api_keys="YOUR_OPENAI_API_KEY", temprerature=0)
-        template = ChatPromptTemplate.from_template("Analyze this data: (data)")
-        prompt = template.format_massages(data=user_imput)
-        response = llm(prompt)
-        st.write(response.content)
+    # Load the OpenAI API key from the environment variable
+    if os.getenv("OPENAI_API_KEY") is None or os.getenv("OPENAI_API_KEY") == "":
+        print("OPENAI_API_KEY is not set")
+        exit(1)
     else:
-        st.warning("Plase enter some data to analyze.")
+        print("OPENAI_API_KEY is set")
+
+    st.set_page_config(page_title="Ask your CSV")
+    st.header("Ask your CSV 📈")
+
+    csv_file = st.file_uploader("Upload a CSV file", type="csv")
+    if csv_file is not None:
+
+        agent = create_csv_agent(
+            OpenAI(temperature=0), csv_file, verbose=True)
+
+        user_question = st.text_input("Ask a question about your CSV: ")
+
+        if user_question is not None and user_question != "":
+            with st.spinner(text="In progress..."):
+                st.write(agent.run(user_question))
+
+
+if __name__ == "__main__":
+    main()
         
